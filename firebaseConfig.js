7a6af0997169ad2b6f8f15ea2d267cf577f5f774
Rana0069 <112808390+Rana0069@users.noreply.github.com>
2025-10-23 15:15:16 +0530

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,24 +1,3 @@
-// PASTE your firebaseConfig object here and export it like below.
-// Example:
-// export const firebaseConfig = {
-//   apiKey: "AIzaSy...",
-//   authDomain: "next-gen-game.firebaseapp.com",
-//   projectId: "next-gen-game",
-//   storageBucket: "next-gen-game.appspot.com",
-//   messagingSenderId: "564928120209",
-//   appId: "1:564928120209:web:..."
-// };
-
-// export const firebaseConfig = {
-//   const: firebaseConfig = {
-//   apiKey: "AIzaSyCkVkBAYCXsuoEaDd6q5TgdOvyPjnRX_hc",
-//   authDomain: "next-gen-game.firebaseapp.com",
-//   projectId: "next-gen-game",
-//   storageBucket: "next-gen-game.firebasestorage.app",
-//   messagingSenderId: "564928120209",
-//   appId: "1:564928120209:web:dba208f944c700048e8630"
-//   },
-// };
 export const firebaseConfig = {
   apiKey: "AIzaSyCkVkBAYCXsuoEaDd6q5TgdOvyPjnRX_hc",
   authDomain: "next-gen-game.firebaseapp.com",
