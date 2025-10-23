6b0077733832f64b2c944d95ee74bee40c128c23
Rana0069 <112808390+Rana0069@users.noreply.github.com>
2025-10-23 15:02:15 +0530

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -9,13 +9,21 @@
 //   appId: "1:564928120209:web:..."
 // };
 
+// export const firebaseConfig = {
+//   const: firebaseConfig = {
+//   apiKey: "AIzaSyCkVkBAYCXsuoEaDd6q5TgdOvyPjnRX_hc",
+//   authDomain: "next-gen-game.firebaseapp.com",
+//   projectId: "next-gen-game",
+//   storageBucket: "next-gen-game.firebasestorage.app",
+//   messagingSenderId: "564928120209",
+//   appId: "1:564928120209:web:dba208f944c700048e8630"
+//   },
+// };
 export const firebaseConfig = {
-  const: firebaseConfig = {
   apiKey: "AIzaSyCkVkBAYCXsuoEaDd6q5TgdOvyPjnRX_hc",
   authDomain: "next-gen-game.firebaseapp.com",
   projectId: "next-gen-game",
   storageBucket: "next-gen-game.firebasestorage.app",
   messagingSenderId: "564928120209",
   appId: "1:564928120209:web:dba208f944c700048e8630"
-  },
 };
